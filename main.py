e7918127453960fe0d719c4e270e9fa552476a88
Denis <den2295404@gmail.com>
2024-05-14 01:40:14 +0300

    Update main.py
    screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
    
    pygame.display.set_caption("Игра ТИР")
    icon = pygame.image.load("")

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 
 pygame.init()
 
+screen = pygame.display.set_mode((SCREEN_W,SCREEN_H))
+
+pygame.display.set_caption("Игра ТИР")
+icon = pygame.image.load("")
 running = True
 while running:
     pass
